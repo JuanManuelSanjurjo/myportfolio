833a4e43ab40ff884d1fab0e253b78cc47eb0dfa
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-15 23:15:47 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://JuanManuelSanjurjo.github.io',
-  base: '/myportfolio',
+  integrations: [react()]
 });
