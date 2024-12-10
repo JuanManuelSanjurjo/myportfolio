f339bed55c0b5024ea6c8074e425ddb9a092cc28
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-12-09 21:04:41 -0300

    nav mobile/desktop script fix

--- src/scripts/navScript.js
+++ src/scripts/navScript.js
@@ -1,20 +1,17 @@
-export function navScript(){
-    const nav = document.querySelector("nav");
-    
-    const navBtn = document.querySelector(".navbtn");
-    const homeLink = document.querySelector("h1 > a > p");
-    const navLinks = document.querySelectorAll("nav > ul > li > a");
+export function navScript() {
+  const nav = document.querySelector("nav");
+  const navBtn = document.querySelector(".navbtn");
+  const homeLink = document.querySelector("h1 > a > p");
+  const navLinks = document.querySelectorAll("nav > ul > li > a");
 
-    const allNavLinks = [...navLinks, navBtn, homeLink];
+  const allNavLinks = [...navLinks, navBtn, homeLink];
 
-    allNavLinks.forEach((link) => {
-      if (link !== null){
-        link.addEventListener("click", () => {
-          nav.classList.toggle("showNav");
-          navBtn.classList.toggle("navBtbEffect");
-        });
-      }
-    })
-
+  allNavLinks.forEach((link) => {
+    if (link !== null) {
+      link.addEventListener("click", () => {
+        nav.classList.toggle("showNav");
+        navBtn.classList.toggle("navBtbEffect");
+      });
+    }
+  });
 }
-
